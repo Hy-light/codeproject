07b6e280325d64e7f6db8c927ac46854895c4659
P_Chime <chimeprince92@gmail.com>
2021-03-22 16:37:50 +0000

    created new repository

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace devAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
